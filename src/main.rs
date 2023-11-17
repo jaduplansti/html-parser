5b1578d8e2f7ce50a9ef9c40d84fb409cfcb9753
jadu lance <simponski@gmail.com>
2023-11-17 14:41:51 +0000

    fixed File structure

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,43 @@ use std::io::BufRead;
 
 struct File {
     fname : String, /* file name */
-    file : fs::File, /* file */
     lines : Vec<String>, /* lines in the file */
 }
 
 impl File {
-    fn new(name : &String, f : fs::File) -> Self {
+    fn new(name : &String) -> Self {
         return File {
             fname : name.clone(),
-            file : f,
             lines : Vec::new(),
         }
     }
 
-    fn read(&mut self) {
-
-        self.lines = reade
+    fn read(&mut self, file : fs::File) {
+        let reader : BufReader<fs::File> = BufReader::new(file);
+        for result in reader.lines() {
+            if let Ok(line) = result {
+                self.lines.push(line);
+            }
+        }
+    }
 
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("usage: ./html-parser [file]");
+        std::process::exit(1);
+    }
+    if let Ok(file) = fs::File::open(&args[1]) {
+        let mut sfile : File = File::new(&args[1]);
+        sfile.read(file);
+        for line in sfile.lines {
+            println!("{}", line);
+        }
+    }
+    else {
+        println!("./html-parser: failed to open '{}'", &args[1]);
+        std::process::exit(1);
+    }
 }
